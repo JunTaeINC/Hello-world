a3606265900012a6c29cc011d9f5ef34f78e1342
JunTaeINC <juntea3523@naver.com>
2022-10-25 23:03:10 +0900

    Commit Three

--- src/practice/Helloworld.java
+++ src/practice/Helloworld.java
@@ -5,6 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Commit Part 2");
+		System.out.println("Commit Part 3");
 	}
 
 }
